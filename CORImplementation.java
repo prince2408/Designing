b3b749d8c62437dbba33b91ac318a918439dbbca
prince2408 <mail2abhi.prince@gmail.com>
2017-02-08 10:23:41 +0530

    Add files via upload

--- CORImplementation.java
+++ CORImplementation.java
@@ -1,5 +1,21 @@
 package design.pattern;
-
+/**
+ * As the name suggests, the chain of responsibility pattern creates a chain of receiver objects for a request. 
+ * This pattern decouples sender and receiver of a request based on type of request.
+ *  This pattern comes under behavioral patterns.
+ *
+ *	In this pattern, normally each receiver contains reference to another receiver. 
+ * If one object cannot handle the request then it passes the same to the next receiver and so on
+ * 
+ * Chain of responsibility pattern is used to achieve loose coupling in software design where a request from client is 
+ * passed to a chain of objects to process them. Then the object in the chain will decide themselves who will 
+ * be processing the request and whether the request is required to be sent to the next object in the chain or not.
+ * 
+ * E.g. includes logger and try-catch block
+ * 
+ * @author abhkumar
+ *
+ */
 public class CORImplementation {
 
 	public static void main(String []args){
